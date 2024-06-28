73b980fadb9f793584a41ee3b5840393d5f1c18a
Gifted Tech <166598671+mouricedevs@users.noreply.github.com>
2024-06-28 07:09:02 +0300

    Gifted

--- gifted.js
+++ gifted.js
@@ -1,1 +1,197 @@
-const _0x2419f8=_0x1935;(function(_0x5adb1e,_0x2fe555){const _0x30cb5d=_0x1935,_0x191e61=_0x5adb1e();while(!![]){try{const _0x4ea74b=-parseInt(_0x30cb5d(0x1da))/0x1*(-parseInt(_0x30cb5d(0x1ec))/0x2)+parseInt(_0x30cb5d(0x1be))/0x3+-parseInt(_0x30cb5d(0x1e9))/0x4+-parseInt(_0x30cb5d(0x1ef))/0x5*(parseInt(_0x30cb5d(0x1e8))/0x6)+parseInt(_0x30cb5d(0x1ce))/0x7*(parseInt(_0x30cb5d(0x1de))/0x8)+parseInt(_0x30cb5d(0x1db))/0x9+parseInt(_0x30cb5d(0x1ad))/0xa*(parseInt(_0x30cb5d(0x1b1))/0xb);if(_0x4ea74b===_0x2fe555)break;else _0x191e61['push'](_0x191e61['shift']());}catch(_0xe0f3d9){_0x191e61['push'](_0x191e61['shift']());}}}(_0x4207,0xe56b1));const qrcode=require('qrcode-terminal'),fs=require('fs'),pino=require('pino'),{default:makeWASocket,Browsers,delay,useMultiFileAuthState,BufferJSON,fetchLatestBaileysVersion,PHONENUMBER_MCC,DisconnectReason,makeInMemoryStore,jidNormalizedUser,makeCacheableSignalKeyStore}=require(_0x2419f8(0x1bf)),Pino=require(_0x2419f8(0x1e5)),NodeCache=require(_0x2419f8(0x1d7)),chalk=require(_0x2419f8(0x1b3)),readline=require(_0x2419f8(0x1c3)),{parsePhoneNumber}=require(_0x2419f8(0x1e6));let phoneNumber=_0x2419f8(0x1c4);const pairingCode=!!phoneNumber||process[_0x2419f8(0x1d2)][_0x2419f8(0x1d9)](_0x2419f8(0x1e1)),useMobile=process['argv'][_0x2419f8(0x1d9)](_0x2419f8(0x1f2)),rl=readline[_0x2419f8(0x1c5)]({'input':process['stdin'],'output':process[_0x2419f8(0x1ca)]}),question=_0x2e297a=>new Promise(_0x47aaab=>rl['question'](_0x2e297a,_0x47aaab));function _0x1935(_0x45ed0c,_0x3f5508){const _0x4207d3=_0x4207();return _0x1935=function(_0x1935a0,_0x342021){_0x1935a0=_0x1935a0-0x1ac;let _0x1fd935=_0x4207d3[_0x1935a0];return _0x1fd935;},_0x1935(_0x45ed0c,_0x3f5508);}async function qr(){const _0x2c960b=_0x2419f8;let {version:_0x1d58ec,isLatest:_0x449c4f}=await fetchLatestBaileysVersion();const {state:_0x58f7c7,saveCreds:_0x5c8d2c}=await useMultiFileAuthState(_0x2c960b(0x1b4)),_0x36454d=new NodeCache(),_0x35973e=makeWASocket({'logger':pino({'level':_0x2c960b(0x1bc)}),'printQRInTerminal':!pairingCode,'browser':Browsers[_0x2c960b(0x1ac)](_0x2c960b(0x1f4)),'auth':{'creds':_0x58f7c7['creds'],'keys':makeCacheableSignalKeyStore(_0x58f7c7['keys'],Pino({'level':_0x2c960b(0x1c0)})[_0x2c960b(0x1d6)]({'level':'fatal'}))},'markOnlineOnConnect':!![],'generateHighQualityLinkPreview':!![],'getMessage':async _0x55172b=>{const _0x3f5192=_0x2c960b;let _0x13688d=jidNormalizedUser(_0x55172b[_0x3f5192(0x1e2)]),_0x356dda=await store[_0x3f5192(0x1c2)](_0x13688d,_0x55172b['id']);return _0x356dda?.[_0x3f5192(0x1e7)]||'';},'msgRetryCounterCache':_0x36454d,'defaultQueryTimeoutMs':undefined});if(pairingCode&&!_0x35973e[_0x2c960b(0x1b6)][_0x2c960b(0x1d1)][_0x2c960b(0x1d5)]){if(useMobile)throw new Error(_0x2c960b(0x1cb));let _0x1e6d77;!!_0x1e6d77?(_0x1e6d77=_0x1e6d77['replace'](/[^0-9]/g,''),!Object[_0x2c960b(0x1f1)](PHONENUMBER_MCC)[_0x2c960b(0x1ee)](_0x111a55=>_0x1e6d77[_0x2c960b(0x1b8)](_0x111a55))&&(console[_0x2c960b(0x1ba)](chalk[_0x2c960b(0x1c9)](chalk['redBright']('Start\x20with\x20country\x20code\x20of\x20your\x20WhatsApp\x20Number,\x20Example\x20:\x20254711111111'))),process[_0x2c960b(0x1c6)](0x0))):(_0x1e6d77=await question(chalk[_0x2c960b(0x1c9)](chalk[_0x2c960b(0x1cc)](_0x2c960b(0x1b2)))),_0x1e6d77=_0x1e6d77[_0x2c960b(0x1d4)](/[^0-9]/g,''),!Object[_0x2c960b(0x1f1)](PHONENUMBER_MCC)[_0x2c960b(0x1ee)](_0x43b82d=>_0x1e6d77['startsWith'](_0x43b82d))&&(console[_0x2c960b(0x1ba)](chalk[_0x2c960b(0x1c9)](chalk[_0x2c960b(0x1ea)](_0x2c960b(0x1b9)))),_0x1e6d77=await question(chalk[_0x2c960b(0x1c9)](chalk[_0x2c960b(0x1cc)](_0x2c960b(0x1b2)))),_0x1e6d77=_0x1e6d77[_0x2c960b(0x1d4)](/[^0-9]/g,''),rl['close']())),setTimeout(async()=>{const _0x63de53=_0x2c960b;let _0x17d8b1=await _0x35973e['requestPairingCode'](_0x1e6d77);_0x17d8b1=_0x17d8b1?.['match'](/.{1,4}/g)?.[_0x63de53(0x1dc)]('-')||_0x17d8b1,console[_0x63de53(0x1ba)](chalk[_0x63de53(0x1d8)](chalk['bgGreen'](_0x63de53(0x1bd))),chalk[_0x63de53(0x1d8)](chalk['white'](_0x17d8b1)));},0xbb8);}_0x35973e['ev']['on'](_0x2c960b(0x1b0),async _0x3758f7=>{const _0x706bbf=_0x2c960b,{connection:_0x2e4e40,lastDisconnect:_0x5bd65d}=_0x3758f7;if(_0x2e4e40==_0x706bbf(0x1e3)){await delay(0x3e8*0xa),await _0x35973e[_0x706bbf(0x1e4)](_0x35973e[_0x706bbf(0x1b5)]['id'],{'text':_0x706bbf(0x1cf)});let _0x22cb85=fs[_0x706bbf(0x1d3)](_0x706bbf(0x1c1));await delay(0x3e8*0x2);const _0x31d71a=await _0x35973e['sendMessage'](_0x35973e[_0x706bbf(0x1b5)]['id'],{'document':_0x22cb85,'mimetype':_0x706bbf(0x1f3),'fileName':'creds.json'});await _0x35973e['sendMessage'](_0x35973e['user']['id'],{'text':_0x706bbf(0x1d0)},{'quoted':_0x31d71a}),await delay(0x3e8*0x2),process[_0x706bbf(0x1c6)](0x0);}_0x2e4e40===_0x706bbf(0x1b7)&&_0x5bd65d&&_0x5bd65d[_0x706bbf(0x1e0)]&&_0x5bd65d['error'][_0x706bbf(0x1ed)][_0x706bbf(0x1ae)]!=0x191&&qr();}),_0x35973e['ev']['on'](_0x2c960b(0x1c8),_0x5c8d2c),_0x35973e['ev']['on'](_0x2c960b(0x1df),()=>{});}qr(),process['on'](_0x2419f8(0x1c7),function(_0x458f4e){const _0x41098c=_0x2419f8;let _0x28620f=String(_0x458f4e);if(_0x28620f[_0x41098c(0x1d9)](_0x41098c(0x1cd)))return;if(_0x28620f[_0x41098c(0x1d9)](_0x41098c(0x1af)))return;if(_0x28620f[_0x41098c(0x1d9)]('Socket\x20connection\x20timeout'))return;if(_0x28620f[_0x41098c(0x1d9)](_0x41098c(0x1eb)))return;if(_0x28620f['includes'](_0x41098c(0x1bb)))return;if(_0x28620f[_0x41098c(0x1d9)](_0x41098c(0x1dd)))return;if(_0x28620f[_0x41098c(0x1d9)](_0x41098c(0x1f0)))return;console[_0x41098c(0x1ba)]('Caught\x20exception:\x20',_0x458f4e);});function _0x4207(){const _0x296936=['libphonenumber-js','message','6PUyDSX','1989744othvYk','redBright','rate-overlimit','442ehNYHx','output','some','9310490lIlPrD','Value\x20not\x20found','keys','--mobile','application/json','Firefox','windows','345480docpuv','statusCode','not-authorized','connection.update','176FcARKU','Please\x20type\x20your\x20WhatsApp\x20number\x20😍\x0aFor\x20example:\x20254711111111\x20:\x20','chalk','./sessions','user','authState','close','startsWith','Start\x20with\x20country\x20code\x20of\x20your\x20WhatsApp\x20Number,\x20Example\x20:\x20254711111111','log','Connection\x20Closed','silent','Your\x20Pairing\x20Code\x20:\x20','3968901nVfyax','@whiskeysockets/baileys','fatal','./sessions/creds.json','loadMessage','readline','254711111111','createInterface','exit','uncaughtException','creds.update','bgBlack','stdout','Cannot\x20use\x20pairing\x20code\x20with\x20mobile\x20api','greenBright','conflict','3981187rDCkMz','\x0a>\x20*DEVICE\x20SUCCESSFULLY\x20LINKED!!!*\x0a\x0a\x0a','\x0a>\x20*ABOVE\x20IS\x20YOUR\x20CREDS.JSON\x20FILE.*\x0a>\x20*USE\x20IT\x20TO\x20DEPLOY\x20YOUR\x20BOT.*\x0a╔═════◇\x0a║\x20『•••\x20𝗩𝗶𝘀𝗶𝘁\x20𝗙𝗼𝗿\x20𝗛𝗲𝗹𝗽\x20•••\x0a❒\x20𝐘𝐨𝐮𝐭𝐮𝐛𝐞:\x20youtube.com/@giftedtechnexus\x0a❒\x20𝐎𝐰𝐧𝐞𝐫:\x20t.me/giftedmd\x0a❒\x20𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥:\x20https://whatsapp.com/channel/0029VaYauR9ISTkHTj4xvi1l\x0a❒\x20𝐆𝐢𝐭𝐡𝐮𝐛:\x20https://github.com/mouricedevs\x0a❒\x20𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫:\x20Gifted\x20Tech\x0a╚═══════════════╝\x0a\x20*𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗\x20𝗩𝟱💜💜💜*\x0a___________________________\x0a-\x20Don\x27t\x20Forget\x20To\x20Fork\x20and\x20Give\x20a\x20Star⭐\x20To\x20My\x20Repo.\x0a-\x20Check\x20Out\x20the\x20YouTube\x20Channel\x20Above\x20for\x20Tutorials.\x0a\x0a\x20','creds','argv','readFileSync','replace','registered','child','node-cache','black','includes','3170zfFtOe','1387665pdjcVM','join','Timed\x20Out','8YDunBw','messages.upsert','error','--pairing-code','remoteJid','open','sendMessage','pino'];_0x4207=function(){return _0x296936;};return _0x4207();}
+function _0x3de7() {
+    const _0x125106 = [
+        '1JfXiIZ',
+        'question',
+        '16BgJXDT',
+        'white',
+        'Value\x20not\x20found',
+        'qrcode-terminal',
+        'registered',
+        '47457969HjmhuB',
+        'match',
+        'exit',
+        'fatal',
+        'chalk',
+        'message',
+        'conflict',
+        '20xQcPwe',
+        'loadMessage',
+        '998596NTrnkq',
+        'Socket\x20connection\x20timeout',
+        'Caught\x20exception:\x20',
+        '254711111111',
+        'bgBlack',
+        'messages.upsert',
+        '\x0a>\x20*ABOVE\x20IS\x20YOUR\x20CREDS.JSON\x20FILE.*\x0a>\x20*USE\x20IT\x20TO\x20DEPLOY\x20YOUR\x20BOT.*\x0a╔═════◇\x0a║\x20『•••\x20𝗩𝗶𝘀𝗶𝘁\x20𝗙𝗼𝗿\x20𝗛𝗲𝗹𝗽\x20•••\x0a❒\x20𝐘𝐨𝐮𝐭𝐮𝐛𝐞:\x20youtube.com/@giftedtechnexus\x0a❒\x20𝐎𝐰𝐧𝐞𝐫:\x20t.me/giftedmd\x0a❒\x20𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥:\x20https://whatsapp.com/channel/0029VaYauR9ISTkHTj4xvi1l\x0a❒\x20𝐆𝐢𝐭𝐡𝐮𝐛:\x20https://github.com/mouricedevs\x0a❒\x20𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫:\x20Gifted\x20Tech\x0a╚═══════════════╝\x0a\x20*𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗\x20𝗩𝟱💜💜💜*\x0a___________________________\x0a-\x20Don\x27t\x20Forget\x20To\x20Fork\x20and\x20Give\x20a\x20Star⭐\x20To\x20My\x20Repo.\x0a-\x20Check\x20Out\x20the\x20YouTube\x20Channel\x20Above\x20for\x20Tutorials.\x0a\x0a\x20',
+        'remoteJid',
+        'Cannot\x20use\x20pairing\x20code\x20with\x20mobile\x20api',
+        '2123622oYvouA',
+        'creds',
+        'application/json',
+        'keys',
+        'redBright',
+        'pino',
+        'requestPairingCode',
+        'Your\x20Pairing\x20Code\x20:\x20',
+        'createInterface',
+        'greenBright',
+        '3bOloCi',
+        'silent',
+        'windows',
+        '--pairing-code',
+        'includes',
+        'creds.json',
+        'join',
+        'bgGreen',
+        'startsWith',
+        'close',
+        '317702IsEBoO',
+        'connection.update',
+        'child',
+        'uncaughtException',
+        '168Taowwm',
+        'authState',
+        '6100QTHqhi',
+        'statusCode',
+        '4172182NjzzRn',
+        'argv',
+        '@whiskeysockets/baileys',
+        'not-authorized',
+        '\x0a>\x20*DEVICE\x20SUCCESSFULLY\x20LINKED!!!*\x0a\x0a\x0a',
+        'Please\x20type\x20your\x20WhatsApp\x20number\x20😍\x0aFor\x20example:\x20254711111111\x20:\x20',
+        'Connection\x20Closed',
+        'readline',
+        '21699RoundW',
+        '2037418ozPmrs',
+        'user',
+        'log',
+        'Firefox',
+        'replace',
+        'output',
+        'black',
+        'Start\x20with\x20country\x20code\x20of\x20your\x20WhatsApp\x20Number,\x20Example\x20:\x20254711111111',
+        'sendMessage',
+        'error'
+    ];
+    _0x3de7 = function () {
+        return _0x125106;
+    };
+    return _0x3de7();
+}
+const _0x29b53a = _0x52a3;
+(function (_0x456768, _0x1837c8) {
+    const _0x7b9b66 = _0x52a3, _0x4eb8ab = _0x456768();
+    while (!![]) {
+        try {
+            const _0x112e58 = -parseInt(_0x7b9b66(0x207)) / 0x1 * (-parseInt(_0x7b9b66(0x1fd)) / 0x2) + -parseInt(_0x7b9b66(0x1e2)) / 0x3 * (parseInt(_0x7b9b66(0x1cf)) / 0x4) + -parseInt(_0x7b9b66(0x1cd)) / 0x5 * (parseInt(_0x7b9b66(0x1d8)) / 0x6) + -parseInt(_0x7b9b66(0x1f4)) / 0x7 * (parseInt(_0x7b9b66(0x209)) / 0x8) + parseInt(_0x7b9b66(0x1fc)) / 0x9 * (-parseInt(_0x7b9b66(0x1f2)) / 0xa) + -parseInt(_0x7b9b66(0x1ec)) / 0xb * (-parseInt(_0x7b9b66(0x1f0)) / 0xc) + parseInt(_0x7b9b66(0x1c6)) / 0xd;
+            if (_0x112e58 === _0x1837c8)
+                break;
+            else
+                _0x4eb8ab['push'](_0x4eb8ab['shift']());
+        } catch (_0x15d6dd) {
+            _0x4eb8ab['push'](_0x4eb8ab['shift']());
+        }
+    }
+}(_0x3de7, 0xb6027));
+const qrcode = require(_0x29b53a(0x20c)), fs = require('fs'), pino = require(_0x29b53a(0x1dd)), {
+        default: makeWASocket,
+        Browsers,
+        delay,
+        useMultiFileAuthState,
+        BufferJSON,
+        fetchLatestBaileysVersion,
+        PHONENUMBER_MCC,
+        DisconnectReason,
+        makeInMemoryStore,
+        jidNormalizedUser,
+        makeCacheableSignalKeyStore
+    } = require(_0x29b53a(0x1f6)), Pino = require(_0x29b53a(0x1dd)), NodeCache = require('node-cache'), chalk = require(_0x29b53a(0x1ca)), readline = require(_0x29b53a(0x1fb)), {parsePhoneNumber} = require('libphonenumber-js');
+let phoneNumber = _0x29b53a(0x1d2);
+function _0x52a3(_0x16e0e2, _0x16df92) {
+    const _0x3de7fb = _0x3de7();
+    return _0x52a3 = function (_0x52a3de, _0x5146e9) {
+        _0x52a3de = _0x52a3de - 0x1c5;
+        let _0x25546d = _0x3de7fb[_0x52a3de];
+        return _0x25546d;
+    }, _0x52a3(_0x16e0e2, _0x16df92);
+}
+const pairingCode = !!phoneNumber || process[_0x29b53a(0x1f5)][_0x29b53a(0x1e6)](_0x29b53a(0x1e5)), useMobile = process[_0x29b53a(0x1f5)]['includes']('--mobile'), rl = readline[_0x29b53a(0x1e0)]({
+        'input': process['stdin'],
+        'output': process['stdout']
+    }), question = _0x296215 => new Promise(_0x3bd527 => rl[_0x29b53a(0x208)](_0x296215, _0x3bd527));
+async function qr() {
+    const _0x23300f = _0x29b53a;
+    let {
+        version: _0x3a61c1,
+        isLatest: _0x3e81d4
+    } = await fetchLatestBaileysVersion();
+    const {
+            state: _0x2e5eee,
+            saveCreds: _0x85cc2e
+        } = await useMultiFileAuthState('./sessions'), _0x1fe093 = new NodeCache(), _0x1ef4a2 = makeWASocket({
+            'logger': pino({ 'level': _0x23300f(0x1e3) }),
+            'printQRInTerminal': !pairingCode,
+            'browser': Browsers[_0x23300f(0x1e4)](_0x23300f(0x200)),
+            'auth': {
+                'creds': _0x2e5eee['creds'],
+                'keys': makeCacheableSignalKeyStore(_0x2e5eee[_0x23300f(0x1db)], Pino({ 'level': _0x23300f(0x1c9) })[_0x23300f(0x1ee)]({ 'level': 'fatal' }))
+            },
+            'markOnlineOnConnect': !![],
+            'generateHighQualityLinkPreview': !![],
+            'getMessage': async _0x503710 => {
+                const _0x68b60f = _0x23300f;
+                let _0x3ae4e0 = jidNormalizedUser(_0x503710[_0x68b60f(0x1d6)]), _0x57e47f = await store[_0x68b60f(0x1ce)](_0x3ae4e0, _0x503710['id']);
+                return _0x57e47f?.[_0x68b60f(0x1cb)] || '';
+            },
+            'msgRetryCounterCache': _0x1fe093,
+            'defaultQueryTimeoutMs': undefined
+        });
+    if (pairingCode && !_0x1ef4a2[_0x23300f(0x1f1)][_0x23300f(0x1d9)][_0x23300f(0x1c5)]) {
+        if (useMobile)
+            throw new Error(_0x23300f(0x1d7));
+        let _0x26afd1;
+        !!_0x26afd1 ? (_0x26afd1 = _0x26afd1['replace'](/[^0-9]/g, ''), !Object[_0x23300f(0x1db)](PHONENUMBER_MCC)['some'](_0xb81456 => _0x26afd1[_0x23300f(0x1ea)](_0xb81456)) && (console[_0x23300f(0x1ff)](chalk[_0x23300f(0x1d3)](chalk[_0x23300f(0x1dc)]('Start\x20with\x20country\x20code\x20of\x20your\x20WhatsApp\x20Number,\x20Example\x20:\x20254711111111'))), process[_0x23300f(0x1c8)](0x0))) : (_0x26afd1 = await question(chalk['bgBlack'](chalk['greenBright'](_0x23300f(0x1f9)))), _0x26afd1 = _0x26afd1[_0x23300f(0x201)](/[^0-9]/g, ''), !Object[_0x23300f(0x1db)](PHONENUMBER_MCC)['some'](_0x544db6 => _0x26afd1['startsWith'](_0x544db6)) && (console[_0x23300f(0x1ff)](chalk[_0x23300f(0x1d3)](chalk[_0x23300f(0x1dc)](_0x23300f(0x204)))), _0x26afd1 = await question(chalk[_0x23300f(0x1d3)](chalk[_0x23300f(0x1e1)](_0x23300f(0x1f9)))), _0x26afd1 = _0x26afd1[_0x23300f(0x201)](/[^0-9]/g, ''), rl[_0x23300f(0x1eb)]())), setTimeout(async () => {
+            const _0x2aa4be = _0x23300f;
+            let _0x974eb9 = await _0x1ef4a2[_0x2aa4be(0x1de)](_0x26afd1);
+            _0x974eb9 = _0x974eb9?.[_0x2aa4be(0x1c7)](/.{1,4}/g)?.[_0x2aa4be(0x1e8)]('-') || _0x974eb9, console[_0x2aa4be(0x1ff)](chalk[_0x2aa4be(0x203)](chalk[_0x2aa4be(0x1e9)](_0x2aa4be(0x1df))), chalk['black'](chalk[_0x2aa4be(0x20a)](_0x974eb9)));
+        }, 0xbb8);
+    }
+    _0x1ef4a2['ev']['on'](_0x23300f(0x1ed), async _0x27f814 => {
+        const _0x1a8280 = _0x23300f, {
+                connection: _0x3f1b18,
+                lastDisconnect: _0x10cc43
+            } = _0x27f814;
+        if (_0x3f1b18 == 'open') {
+            await delay(0x3e8 * 0xa), await _0x1ef4a2[_0x1a8280(0x205)](_0x1ef4a2[_0x1a8280(0x1fe)]['id'], { 'text': _0x1a8280(0x1f8) });
+            let _0x2b8dbf = fs['readFileSync']('./sessions/creds.json');
+            await delay(0x3e8 * 0x2);
+            const _0x2e06ce = await _0x1ef4a2[_0x1a8280(0x205)](_0x1ef4a2[_0x1a8280(0x1fe)]['id'], {
+                'document': _0x2b8dbf,
+                'mimetype': _0x1a8280(0x1da),
+                'fileName': _0x1a8280(0x1e7)
+            });
+            await _0x1ef4a2['sendMessage'](_0x1ef4a2[_0x1a8280(0x1fe)]['id'], { 'text': _0x1a8280(0x1d5) }, { 'quoted': _0x2e06ce }), await delay(0x3e8 * 0x2), process[_0x1a8280(0x1c8)](0x0);
+        }
+        _0x3f1b18 === _0x1a8280(0x1eb) && _0x10cc43 && _0x10cc43[_0x1a8280(0x206)] && _0x10cc43[_0x1a8280(0x206)][_0x1a8280(0x202)][_0x1a8280(0x1f3)] != 0x191 && qr();
+    }), _0x1ef4a2['ev']['on']('creds.update', _0x85cc2e), _0x1ef4a2['ev']['on'](_0x23300f(0x1d4), () => {
+    });
+}
+qr(), process['on'](_0x29b53a(0x1ef), function (_0x31e8d2) {
+    const _0x39dba8 = _0x29b53a;
+    let _0x21d935 = String(_0x31e8d2);
+    if (_0x21d935[_0x39dba8(0x1e6)](_0x39dba8(0x1cc)))
+        return;
+    if (_0x21d935['includes'](_0x39dba8(0x1f7)))
+        return;
+    if (_0x21d935[_0x39dba8(0x1e6)](_0x39dba8(0x1d0)))
+        return;
+    if (_0x21d935[_0x39dba8(0x1e6)]('rate-overlimit'))
+        return;
+    if (_0x21d935[_0x39dba8(0x1e6)](_0x39dba8(0x1fa)))
+        return;
+    if (_0x21d935['includes']('Timed\x20Out'))
+        return;
+    if (_0x21d935[_0x39dba8(0x1e6)](_0x39dba8(0x20b)))
+        return;
+    console[_0x39dba8(0x1ff)](_0x39dba8(0x1d1), _0x31e8d2);
+});
